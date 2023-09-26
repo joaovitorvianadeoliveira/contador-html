8ef1a6da6b90c16d01b26ea182d8577a431fb102
joaovitorvianadeoliveira <joaovitorop2001@gmail.com>
2023-09-26 20:47:27 -0300

    feat: create functions in script

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+const h2 = document.querySelector("h2");
+
+function aumentar(){
+    // h2.innerText = h2.innerText + 1
+    // h2.innerText = +h2.innerText + 1 
+    h2.innerText = parseInt(h2.innerText) + 1
+    
+}
+
+function zerar(){
+h2.innerText = 0
+}
+
+function diminuir(){
+    // h2.innertext = h2.innertext - 1
+    h2.innerText -= 1
+}
